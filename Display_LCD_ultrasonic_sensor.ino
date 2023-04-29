6fa5fa146d1f1c72ab28276ee38a683faaea8e7e
CarlosJaynor <jaynor1991@gmail.com>
2023-04-29 10:55:00 +0200

    Add files via upload

---
+++ Display_LCD_ultrasonic_sensor.ino
@@ -0,0 +1,48 @@
+#include <LiquidCrystal.h>
+
+//Variables display LCD
+int rs = 12;
+int e  = 11;
+int d4 = 5;
+int d5 = 4;
+int d6 = 3;
+int d7 = 2;
+
+//variables Ultrasonic sensor
+const int trigPin = 9;
+const int echoPin = 10;
+
+LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
+
+void setup() {
+  
+  lcd.begin(16, 2);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.034 / 2;  // Calcular la distancia en cm
+
+  lcd.clear();  // Limpiar el display
+  lcd.setCursor(0, 0);  // Establecer la posición del cursor
+  lcd.print("Distancia: ");
+  lcd.print(distance);
+  lcd.print(" cm");
+
+  Serial.print("Distancia: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(500);  // Esperar medio segundo antes de medir de nuevo
+}
